3782a3239ecd9ac0142134ca39b545140eb61659
{:_-SILENT-_:} <alvinasshak@gmail.com>
2026-03-20 15:48:47 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,1 @@
-self.addEventListener('push', e => {
-  const data = e.data ? e.data.json() : {};
-  self.registration.showNotification(data.title || 'NEXUS', {
-    body: data.body || 'New message',
-    icon: '/Nexus-Chat/icon-192.png',
-    tag: 'nexus-msg',
-    renotify: true,
-    vibrate: [200, 100, 200]
-  });
-});
-self.addEventListener('notificationclick', e => {
-  e.notification.close();
-  e.waitUntil(clients.openWindow('/Nexus-Chat/'));
-});
-self.addEventListener('fetch', e => {
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
-});
+importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
